d94897b0dc709dafc5078f119b1b075dd3926873
anshi-debata-01 <141932221+anshi-debata-01@users.noreply.github.com>
2024-02-05 00:13:14 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+a = 50
+b = 2
+print("addition of a and b",a+b)
+print("subtraction of a and b",a-b)
+print("multiplication of a and b",a*b)
+print("division of a and b",a/b)
+print("modulus of a and b",a%b)
+print("floor div of a and b",a//b)
+print("power of a and b",a**b)
